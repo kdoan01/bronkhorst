47cadad68cfc0738212e6913c21cf36012d6c2d6
kdoan01 <60363777+kdoan01@users.noreply.github.com>
2020-01-27 14:52:58 -0500

    Update stop.py

--- stop.py
+++ stop.py
@@ -4,4 +4,6 @@ import propar
 # Connect to the local instrument, when no settings provided
 # defaults to locally connected instrument (address=0x80, baudrate=38400)
 cori_flow = propar.instrument('COM12')
+
+#Set the setpoint to 0 when wanting to stop the pump
 cori_flow.setpoint = 0
